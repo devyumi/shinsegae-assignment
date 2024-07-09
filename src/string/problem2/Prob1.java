1b4b50c3da952a9603fff5ad65291109a22deabf
devyumi <yyumiwing@naver.com>
2024-07-09 16:36:19 +0900

    submit string assignment problem2

---
+++ src/string/problem2/Prob1.java
@@ -0,0 +1,34 @@
+package string.problem2;
+
+public class Prob1 {
+
+  public static void main(String[] args) {
+    System.out.println("문자열에 특정문자 변경하는 테스");
+    System.out.println("-------------Sample 1 --------------");
+    String str1 = myReplace("hello world", 'l', '*');
+    System.out.println(str1);
+
+    System.out.println("-------------Sample 2 --------------");
+    String str2 = myReplace("hello world", ' ', '-');
+    System.out.println(str2);
+
+    System.out.println("-------------Sample 3 --------------");
+    String str3 = myReplace("hello world", 'a', '*');
+    System.out.println(str3);
+
+
+  }
+
+  public static String myReplace(String str, char oldChar, char newChar) {
+    char[] chars = str.toCharArray();
+    StringBuilder sb = new StringBuilder();
+
+    for (char c : chars) {
+      if (c == oldChar) {
+        c = newChar;
+      }
+      sb.append(c);
+    }
+    return sb.toString();
+  }
+}
